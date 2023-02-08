018ee91bc4e2db898c03c36e45bae832453f3fa0
hqh66 <101696682+hqh66@users.noreply.github.com>
2023-02-08 23:58:46 +0800

    Update AllConfig.java

--- src/main/java/com/pt/vx/config/AllConfig.java
+++ src/main/java/com/pt/vx/config/AllConfig.java
@@ -20,11 +20,11 @@ import java.util.List;
  *               -- 2022/10/5
  */
 public class AllConfig {
-    public static final String cron = "0 0 8 * * ?"; //分别代表：秒、分、时、日、月、星期
-    public static final String VxAppId = "微信的APPID";
-    public static final String VxAppSecret = "微信的密钥";
-    public static final String WeatherKey = "高德地图key";
-    public static final String WeatherKey_HeFeng = "和风天气KEY";
+    public static final String cron = "0 0 0 * * ?"; //分别代表：秒、分、时、日、月、星期
+    public static final String VxAppId = "wxb6503063be24a0d4";
+    public static final String VxAppSecret = "351f4e56e81847eb5a2b2ed35d384363";
+    public static final String WeatherKey = "16e701f8541efda69a0334c2cd5125cc";
+    public static final String WeatherKey_HeFeng = "064b081beab04011b2ecf1570ab1df81";
 
     //下列所有填写的  true 为开启， false 为不开启 ，#XXXXXX是颜色16进制HEX码（不知道颜色16进制HEX码可以百度）
     //系统开关
@@ -73,27 +73,24 @@ public class AllConfig {
         //  注意：日期里面的数字，填正常的数字就行了.比如1就是1，不要填01
         //  注意：每个用户信息的最后一项不需要加逗号！！！
         userList.add(getUser(
-                "这个人的微信号", //扫码关注你的测试号以后，测试平台会出现TA的微信号
-                "模板ID", //要给这个人发送的模板ID
-                "pt", //咋称呼这个人
-                "江苏省南京市玄武区", //这个人的详细地址
-                "南京", //这个人在的城市
-                new BirthDay(1999,2,15,true,false), //分别代表年、月、日、是否是农历(true为农历、false为公历)、是否统计天数(true为统计，false为倒计时)
-                new BirthDay(1999,8,11,false,false),
-                new BirthDay(2020,7,8,true,true),
-                new BirthDay(2020,7,8,true,false)
-        ));
+                "oV7026C-dcRYRGzyX4QoxFSbAYdk", //扫码关注你的测试号以后，测试平台会出现TA的微信号
+                "Kktk8IbVJOyjHuN0BuCGrr-kzGppwoCTHTH_KU53hmk", //要给这个人发送的模板ID
+                "华华居", //咋称呼这个人
+                "广东省广州市越秀区", //这个人的详细地址
+                "广州", //这个人在的城市
+               // new BirthDay(1997,10,18,true,false), 
+             //   new BirthDay(1999,8,11,false,false),
+              //  new BirthDay(2020,7,8,true,true),
+                new BirthDay(1997,6,20,false,false)
+        ));//分别代表年、月、日、是否是农历(true为农历、false为公历)、是否统计天数(true为统计，false为倒计时)
 
         userList.add(getUser(
-                "这个人扫码后的微信号",//扫码关注你的测试号以后，测试平台会出现TA的微信号
-                "微信消息模板ID",//要给这个人发送的模板ID
-                "这个人的称呼",//咋称呼这个人
-                "江苏省南京市玄武区",//这个人的详细地址
-                "南京",//这个人在的城市
-                new BirthDay(1999,8,11,false,false),
-                new BirthDay(1999,2,15,true,false),
-                new BirthDay(2020,7,8,true,true),
-                new BirthDay(2020,7,8,true,false)
+                "oV7026C-dcRYRGzyX4QoxFSbAYdk",//扫码关注你的测试号以后，测试平台会出现TA的微信号
+                "Kktk8IbVJOyjHuN0BuCGrr-kzGppwoCTHTH_KU53hmk",//要给这个人发送的模板ID
+                "小可爱",//咋称呼这个人
+                "广西壮族自治区南宁市良庆区",//这个人的详细地址
+                "南宁",//这个人在的城市
+                new BirthDay(1997,10,18,true,false)
         ));
 
 
